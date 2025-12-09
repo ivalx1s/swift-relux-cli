394b942dfe04b017de73f6583208d7f42aaa3e94
Ivan Oparin <oparin@me.com>
2025-12-09 04:31:28 +0400

    Make entry point async

--- Sources/relux/Relux.swift
+++ Sources/relux/Relux.swift
@@ -1,10 +1,14 @@
 import ArgumentParser
 
 @main
-struct Relux: ParsableCommand {
+struct Relux: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "relux",
         abstract: "Relux CLI tool.",
         subcommands: []
     )
+
+    func run() async throws {
+        // Async entry point ready
+    }
 }
